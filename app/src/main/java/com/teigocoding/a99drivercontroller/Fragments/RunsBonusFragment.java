4b2a000ae1d02c2876aab19c87a7caaf9cd00f29
Tiago Frota Vallim <tiago.vallim1992@gmail.com>
2022-05-01 21:23:51 -0300

    Commit de menus e runsmenu

---
+++ app/src/main/java/com/teigocoding/a99drivercontroller/Fragments/RunsBonusFragment.java
@@ -0,0 +1,66 @@
+package com.teigocoding.a99drivercontroller.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.teigocoding.a99drivercontroller.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link RunsBonusFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RunsBonusFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public RunsBonusFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment RunsBonusFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static RunsBonusFragment newInstance(String param1, String param2) {
+        RunsBonusFragment fragment = new RunsBonusFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_runs_bonus, container, false);
+    }
+}
--- app/src/main/java/com/teigocoding/a99drivercontroller/MenuRunsActivity.java
+++ app/src/main/java/com/teigocoding/a99drivercontroller/MenuRunsActivity.java
@@ -1,14 +1,40 @@
 package com.teigocoding.a99drivercontroller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
 public class MenuRunsActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavHostFragment navHostFragment;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_runs);
+
+
+
+        initNavigation();
+
     }
+
+    private void initNavigation(){
+
+        bottomNavigationView = findViewById(R.id.bottomnav_runs);
+
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+    }
+
 }
