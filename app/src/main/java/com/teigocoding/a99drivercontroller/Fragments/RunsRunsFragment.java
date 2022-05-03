d768b16653dcccacf9d106093255c40e2c7785e0
Tiago Frota Vallim <tiago.vallim1992@gmail.com>
2022-05-03 15:43:38 -0300

    Update RunsRunsFragment.java

--- app/src/main/java/com/teigocoding/a99drivercontroller/Fragments/RunsRunsFragment.java
+++ app/src/main/java/com/teigocoding/a99drivercontroller/Fragments/RunsRunsFragment.java
@@ -10,34 +10,20 @@ import android.view.ViewGroup;
 
 import com.teigocoding.a99drivercontroller.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RunsRunsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class RunsRunsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     public RunsRunsFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RunsRunsFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static RunsRunsFragment newInstance(String param1, String param2) {
         RunsRunsFragment fragment = new RunsRunsFragment();
