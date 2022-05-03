4a46310efb4410414f4e9279eb03d6fb55dfc39a
Tiago Frota Vallim <tiago.vallim1992@gmail.com>
2022-05-03 15:37:25 -0300

    commit 3

--- app/src/main/java/com/teigocoding/a99drivercontroller/Fragments/RunsBonusFragment.java
+++ app/src/main/java/com/teigocoding/a99drivercontroller/Fragments/RunsBonusFragment.java
@@ -7,22 +7,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 import com.teigocoding.a99drivercontroller.R;
+import com.teigocoding.a99drivercontroller.databinding.RunsItemBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RunsBonusFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class RunsBonusFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+    Spinner spinner_total;
+
+
     private String mParam1;
     private String mParam2;
 
@@ -30,15 +29,7 @@ public class RunsBonusFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RunsBonusFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static RunsBonusFragment newInstance(String param1, String param2) {
         RunsBonusFragment fragment = new RunsBonusFragment();
         Bundle args = new Bundle();
